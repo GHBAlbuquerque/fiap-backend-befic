6fc3912707a724cc2db3d0d039f3438e44aab3b4
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2022-11-05 18:25:13 -0300

    initial configuration

---
+++ src/main/java/br/com/fiap/befic/BeficApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.befic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeficApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeficApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/befic/BeficApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.befic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeficApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
